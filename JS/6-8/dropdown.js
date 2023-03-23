65983b259fe11c26cd8ce9dcc0986dd079772f40
Demasto <mr.d1men@yandex.ru>
2023-03-23 19:35:56 +0300

    add 4 lab (html/css)

--- JS/6-8/dropdown.js
+++ JS/6-8/dropdown.js
@@ -1,7 +1,15 @@
+
+
 function addItem() {
     let newItemText = document.getElementById("newItemText").value;
     let newItemValue = document.getElementById("newItemValue").value;
     let line = `${newItemText} ${newItemValue}`
     let lineHTML = `<option value=${newItemValue}>${line}</option>`
     document.getElementById("menu").innerHTML += lineHTML;
+   
+    
+   
+}
+function deleteItem() {
+   
 }
